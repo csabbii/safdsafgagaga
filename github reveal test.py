aaef634b92f6ff6a2bd88f5cb9fa11e35309678f
csabbii <103537866+csabbii@users.noreply.github.com>
2022-04-14 21:01:46 +0200

    Add files via upload

---
+++ github reveal test.py
@@ -0,0 +1,37 @@
+{
+    "name": "Roli #1",
+    "symbol": "RLI",
+    "image": "0.png",
+    "description": "rudi",
+    "external_url": "https://www.rudi.com/",
+    "seller_fee_basis_points": 888.0000000000001,
+    "collection": {
+        "name": "rudi",
+        "family": "Roli"
+    },
+    "properties": {
+        "files": [
+            {
+                "uri": "0.png",
+                "type": "image/png"
+            }
+        ],
+        "category": "image",
+        "creators": [
+            {
+                "address": "G3XE6c1dvwF59EEo8hUsMRg4ucgcJCYSD4bttkPUF6Ym",
+                "share": 40
+            },
+            {
+                "address": "6ybYWysPcnNYyH8gZSuQVaVDku7mbkrWyXdHw7xqXBBn",
+                "share": 60
+            }
+        ]
+    },
+    "attributes": [
+        {
+            "trait_type": "Roli",
+            "value": "sziaroli"
+        }
+    ]
+}
